a12e134f4c4deb9fee48118b41c06f1a95083b24
emkooz <1472279+emkooz@users.noreply.github.com>
2023-02-08 17:58:49 -0800

    Skeleton helpers + isolating MP implementation

--- public/scripts/game/Bone.js
+++ public/scripts/game/Bone.js
@@ -8,33 +8,37 @@ export default class Bone extends VectorGameObject {
 	];
 
 	style = {
-		stroke: 255,
+		stroke: this.p5.color(123, 234, 100),
 		strokeWeight: 5,
 	};
 
 	name = "unknown";
+	nameFormatted = "Unknown";
+	index = -1;
+	points = [-1, -1];
 
-	constructor(name) {
+	constructor(name, nameFormatted, index, points) {
 		// constructor for vector object, no position yet
 		super(0, 0, {}, true, 5, 255, 0, 1, 1, false);
 
 		this.name = name;
+		this.nameFormatted = nameFormatted;
+		this.index = index;
+		this.points = points;
 	}
 
-	update() {
+	update(vertices) {
 		// update bone position from current prediction
-		if (this.name !== "unknown") {
-			const prediction = Mediapipe.getInstance().getBoneVertices(this.name);
+		if (this.name !== "unknown" && vertices) {
+			// const prediction = Mediapipe.getInstance().getBoneVertices(this.name);
 
-			if (prediction) {
-				this.vertices = prediction;
+			this.vertices = vertices;
 
-				// vertices are normalized to 0 - 2, scale to canvas
-				this.vertices.forEach(vertex => {
-					vertex.x *= (this.gameSession.canvasWidth / 2);
-					vertex.y *= (this.gameSession.canvasHeight / 2);
-				});
-			}
+			// vertices are normalized to 0 - 2, scale to canvas
+			this.vertices.forEach(vertex => {
+				vertex.x *= (this.gameSession.canvasWidth / 2);
+				vertex.y *= (this.gameSession.canvasHeight / 2);
+			});
 		}
 	}
 
@@ -42,5 +46,9 @@ export default class Bone extends VectorGameObject {
 		this.p5.stroke(this.style.stroke);
 		this.p5.strokeWeight(this.style.strokeWeight);
 		this.p5.line(this.vertices[0].x, this.vertices[0].y, this.vertices[1].x, this.vertices[1].y)
+	}
+
+	setColor(r, g, b) {
+		this.style.stroke = this.p5.color(`rgba(${r},${g},${b},1)`);
 	}
 }
