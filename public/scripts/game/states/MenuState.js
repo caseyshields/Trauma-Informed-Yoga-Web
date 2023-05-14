0e2b9967aa9d7420d7067570c701a9c68609203c
smshields <samuel.m.shields@gmail.com>
2023-05-13 19:46:21 -0700

    Added back button to menu state

--- public/scripts/game/states/MenuState.js
+++ public/scripts/game/states/MenuState.js
@@ -1,4 +1,5 @@
 import State from "../../core/State.js";
+import BackButton from "../buttons/BackButton.js";
 
 /** Example of Gamestate
  *
@@ -12,22 +13,65 @@ import State from "../../core/State.js";
  */
 
 export default class MenuState extends State {
-	
+	backButton = {};
+
 	constructor() {
 		super("Menu");
-		//check
+		
 	}
 
 	setup() {
 		super.setup();
+
+        //Instantiate backbutton
+		let backButtonLayout = {
+			x: this.gameSession.canvasWidth * .05,
+			y: this.gameSession.canvasHeight * .05,
+			width: this.gameSession.canvasWidth * .05,
+			height: this.gameSession.canvasWidth * .05
+		}
+
+		let backButtonStyle = {
+			stroke: this.p5.color(255, 255, 255),
+			strokeWeight: 5,
+			fill: this.p5.color(0, 0, 0),
+			hoverFill: this.p5.color(123, 123, 123),
+			pressedFill: this.p5.color(255, 255, 255),
+			loadingFill: this.p5.color(62, 62, 62),
+			disabledFill: this.p5.color(125, 0, 0),
+		}
+
+		this.backButton = new BackButton(backButtonLayout, backButtonStyle, false, "Game");
+		this.backButton.setup();
 	}
 
 	render() {
 		super.render();
+
+        //UI
+        this.backButton.render();
 	}
 
 	update() {
 		super.update();
+
+        //UI
+        this.backButton.render();
+
+        this.backButton.resize(
+			this.gameSession.canvasWidth * .05,
+			this.gameSession.canvasHeight * .05,
+			this.gameSession.canvasWidth * .05,
+			this.gameSession.canvasWidth * .05
+		);
+	}
+
+    mousePressed(){
+		this.backButton.checkPressed();
+	}
+
+	mouseReleased(){
+		this.backButton.checkReleased();
 	}
 	
 
