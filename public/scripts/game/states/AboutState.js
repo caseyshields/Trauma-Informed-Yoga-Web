c109dfb329a8141c2686fef902a908ace5e14b17
sonali-7 <99060458+sonali-7@users.noreply.github.com>
2023-08-01 13:13:16 -0700

    about page button framework

--- public/scripts/game/states/AboutState.js
+++ public/scripts/game/states/AboutState.js
@@ -1,8 +1,13 @@
 import State from "../../core/State/State.js";
 import BackButton from "../buttons/BackButton.js";
+import Button from "../../core/UI/Button.js";
 
 export default class AboutState extends State {
     backButton = {};
+    tsyButton = {};
+    yogaButton = {};
+    howGameButton = {};
+    creditsButton = {};
 
 
     constructor() {
@@ -32,6 +37,94 @@ export default class AboutState extends State {
 
         this.backButton = new BackButton(backButtonLayout, backButtonStyle, false, "Loading");
         this.backButton.setup();
+
+        let tsyButtonLayout = {
+            xRatio: 0.525,
+            yRatio: 0.75,
+            widthRatio: 0.4,
+            heightRatio: 0.2,
+        };
+    
+        let tsyButtonStyle = {
+            stroke: this.p5.color(230, 251, 255),
+            strokeWeight: 5,
+            fill: this.p5.color(51, 51, 51),
+            hoverFill: this.p5.color(76, 76, 76),
+            pressedFill: this.p5.color(102, 102, 102),
+        };
+    
+        let tsyButtonText = {
+            text: "TSY resources",
+            textRatio: 10,
+        };
+    
+        this.tsyButton = new Button(tsyButtonLayout, tsyButtonStyle, tsyButtonText);
+
+        let yogaButtonLayout = {
+            xRatio: 0.075,
+            yRatio: 0.75,
+            widthRatio: 0.4,
+            heightRatio: 0.2,
+        };
+    
+        let yogaButtonStyle = {
+            stroke: this.p5.color(230, 251, 255),
+            strokeWeight: 5,
+            fill: this.p5.color(51, 51, 51),
+            hoverFill: this.p5.color(76, 76, 76),
+            pressedFill: this.p5.color(102, 102, 102),
+        };
+    
+        let yogaButtonText = {
+            text: "Yoga: Culture and History",
+            textRatio: 10,
+        };
+    
+        this.yogaButton = new Button(yogaButtonLayout, yogaButtonStyle, yogaButtonText);
+
+        let howGameButtonLayout = {
+            xRatio: 0.075,
+            yRatio: 0.5,
+            widthRatio: 0.4,
+            heightRatio: 0.2,
+        };
+    
+        let howGameButtonStyle  = {
+            stroke: this.p5.color(230, 251, 255),
+            strokeWeight: 5,
+            fill: this.p5.color(51, 51, 51),
+            hoverFill: this.p5.color(76, 76, 76),
+            pressedFill: this.p5.color(102, 102, 102),
+        };
+    
+        let howGameButtonText = {
+            text: "How This Game Works",
+            textRatio: 10,
+        };
+    
+        this.howGameButton = new Button(howGameButtonLayout, howGameButtonStyle, howGameButtonText);
+
+        let creditsButtonLayout = {
+            xRatio: 0.525,
+            yRatio: 0.5,
+            widthRatio: 0.4,
+            heightRatio: 0.2,
+        };
+    
+        let creditsButtonStyle  = {
+            stroke: this.p5.color(230, 251, 255),
+            strokeWeight: 5,
+            fill: this.p5.color(51, 51, 51),
+            hoverFill: this.p5.color(76, 76, 76),
+            pressedFill: this.p5.color(102, 102, 102),
+        };
+    
+        let creditsButtonText = {
+            text: "Academic Credits",
+            textRatio: 10,
+        };
+    
+        this.creditsButton = new Button(creditsButtonLayout, creditsButtonStyle, creditsButtonText);
     }
 
     render() {
@@ -39,6 +132,10 @@ export default class AboutState extends State {
 
         //UI
         this.backButton.render();
+        this.tsyButton.render();
+        this.yogaButton.render();
+        this.howGameButton.render();
+        this.creditsButton.render();
     }
 
     resize() {
@@ -50,6 +147,11 @@ export default class AboutState extends State {
             this.gameSession.canvasWidth * .05,
             this.gameSession.canvasWidth * .05
         );
+
+        this.tsyButton.updateSize();
+        this.yogaButton.updateSize();
+        this.howGameButton.updateSize();
+        this.creditsButton.updateSize();
     }
 
     update() {
@@ -57,10 +159,29 @@ export default class AboutState extends State {
 
         //UI
         this.backButton.update();
+        this.tsyButton.update();
+        this.yogaButton.update();
+        this.howGameButton.update();
+        this.creditsButton.update();
+    }
+
+    mousePressed(){
+        this.backButton.checkPressed();
+        this.tsyButton.checkPressed();
+        this.yogaButton.checkPressed();
+        this.howGameButton.checkPressed();
+        this.creditsButton.checkPressed();
     }
 
     mouseReleased(){
         this.backButton.checkReleased();
+        this.tsyButton.checkReleased(() => {
+            this.gameSession.setCurrentStateByName("Loading");
+//          alert("This will launch the about page!");
+        });
+        this.yogaButton.checkReleased();
+        this.howGameButton.checkReleased();
+        this.creditsButton.checkReleased();
     }
     
 
