e7eff5de7f0cb901337902b7f5f3ec664a9dc2cc
caseyshields <33648529+caseyshields@users.noreply.github.com>
2024-04-21 20:12:26 -0700

    WIP

---
+++ public/scripts/game/form/joystick.js
@@ -0,0 +1,9 @@
+import GameObject from "../../core/GameObject/GameObject.js";
+
+/** trying to make a input target that produces a vector */
+export default class Target extends GameObject {
+
+    origin;
+    landmark;
+
+}
